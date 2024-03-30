6fc7dda5fb0d957db19e73c5e119c3e3693fc229
imohorcic <imohorcic@loveat.com.ar>
2024-03-29 21:41:45 -0300

    Create Graphic.jsx

---
+++ src/components/Graphic/Graphic.jsx
@@ -0,0 +1,19 @@
+const Graphic = ({}) => {
+    /*
+        TODO:
+        - Tiene que recibir: 
+            - array de coordenadas, con colores. ej: {x: , y: , color: }
+            - Cuantas lineas horizontales va a tener que trazar
+            - Booleano para que arranque en 0 el grafico, o tome la cota menor del grafico.
+            - labels de lineas de ejes.
+            - Recibe un id, un className, y un style.
+            - Recibe un height y un width: Puede recibir pixeles en formato numero, osea "600", "1800", o puede recibir porcentajes, en cuyo caso, adaptar el grafico a lo que vaya a ocupar con ese estilo, en formato: "50%".
+        - Que tiene que hacer
+            - Obtener el maximo ponderado y el minimo ponderado, siendo ambos multiplos de la cantidad de lineas horizontales. NTH: el ponderado menor que sea multiplo
+            - Trazar las lineas horizontales de fondo al grafico de punta a punta de este 
+            - Trazar lineas de ejes con sus labels.
+            - Que haga el grafico.
+    */
+}
+
+export default Graphic;
